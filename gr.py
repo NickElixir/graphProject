d58a6f04d33fa928b8556119cafcb95cb3c17074
mikekub <misha.kubasov@gmail.com>
2016-07-26 16:32:38 +0400

    Add files via upload

--- gr.py
+++ gr.py
@@ -1,3 +1,5 @@
+import random
+
 class Vertex:
     def __init__(self, x=0.0, y=0.0, z=0.0, color='white', rad=5, q=1.0, vx=0.0, vy=0.0, vz=0.0):
         self.x = x
@@ -11,7 +13,7 @@ class Vertex:
         self.vz = vz
         self.id = 0.0
 class Edge:
-    def __init__(self, v1, v2, l=0.0, k=0.0, color='white'):
+    def __init__(self, v1:Vertex, v2:Vertex, l=0.0, k=0.0, color='white'):
         self.v1 = v1
         self.v2 = v2
         self.l = l
@@ -25,7 +27,7 @@ class Graph:
     def addVertex(self):
         v = Vertex()
         self.V.append(v)
-    def addEdge(self, v1, v2):
+    def addEdge(self, v1:Vertex, v2:Vertex):
         e = Edge(v1, v2)
         e2 = Edge(v2, v1)
         if self.V.count(v1) < 1 or self.V.count(v2) < 1 or v1 == v2:
@@ -37,16 +39,16 @@ class Graph:
         else:
             self.E.append(e)
             return 0
-    def delVertex(self, v):
+    def delVertex(self, v:Vertex):
         countV = self.V.count(v)
-        if count == 0:
+        if countV == 0:
             print('Error: No such vertex')
             return -1
         else:
             for e in E:
                 if v == e.v1 or v == e.v2:
-                    delEdge(e.v1, e.v2)
-                    self.V.pop(V.index(v))
+                    self.delEdge(e.v1, e.v2)
+                    self.V.remove(v)
             return 0
     def delEdge(self, v1:Vertex, v2:Vertex):
         allBad = True
@@ -62,8 +64,23 @@ class Graph:
         self.E = []
     def clearAll(self):
         self.V = []
-        makeEmpty()
-    def createEmpty(self, n):
+        self.makeEmpty()
+    def createEmpty(self, n:int):
         clearAll()
         for i in range(n):
             addVertex()
+    def randomTree(self, n:int):
+        self.clearAll()
+        self.addVertex()
+        count = 1
+        for i in range(n):
+            self.addVertex()
+            count += 1
+            k = random.randrange(0, count-1)
+            while count-1 == k:
+                k = random.randrange(0, count-1)
+            self.addEdge(self.V[count-1], self.V[k])
+            print(count-1, k)
+
+graph = Graph()
+graph.randomTree(10)
